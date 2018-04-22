6807d9ff6bc094292c96002461a8a8b9e2b77556
Luke Meszar <lume0018@colorado.edu>
2018-04-22 14:28:30 -0600

    Changes

--- HW6/luke.meszar.5832.hw6.py
+++ HW6/luke.meszar.5832.hw6.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from collections import OrderedDict
 import numpy as np
+from sklearn.preprocessing import normalize
+from sklearn.cluster import KMeans
 
 def parse_text():
     lines = [line.strip() for line in open('wsj00-18.tag') if "\t" in line]
@@ -24,7 +26,7 @@ def get_top_1000(words):
 def create_left_right_vecs(words, unique_words, top_words):
     left_vec  = [ [0]*len(unique_words) ]*len(top_words)
     right_vec = [ [0]*len(unique_words) ]*len(top_words)
-    print("len(words),", len(words))
+    print("creating vectors")
     for i in range(len(words)):
         current_word = words[i]
         if current_word in top_words:
@@ -32,14 +34,20 @@ def create_left_right_vecs(words, unique_words, top_words):
             right_word = words[(i+1) % len(words)]
             left_vec[top_words.index(current_word)][unique_words.index(left_word)] += 1
             right_vec[top_words.index(current_word)][unique_words.index(right_word)] += 1
-    return left_vec, right_vec
+    combined_vectors = []
+    for i in range(len(left_vec)):
+        combined_vectors.append(left_vec[i]+right_vec[i])
+    return combined_vectors
 
+def normalize_and_fit(vectors):
+    print("normalizing")
+    normalized_vectors = normalize(np.array(vectors), axis=1, norm='l1')
+    print("kmeans")
+    kmeans = KMeans(n_clusters=25, random_state=0).fit(normalized_vectors)
+    print(kmeans.labels_)
 
 if __name__ == '__main__':
     words, unique_words = parse_text()
     top_words = get_top_1000(words)
-    left_vec, right_vec = create_left_right_vecs(words, unique_words, top_words)
-    for row in left_vec:
-        for entry in row:
-            if entry != 0:
-                print(entry)
+    vectors = create_left_right_vecs(words, unique_words, top_words)
+    normalize_and_fit(vectors)
